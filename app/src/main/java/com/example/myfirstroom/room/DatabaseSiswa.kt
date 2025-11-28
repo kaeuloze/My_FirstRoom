db68be9ca7dc85addca03fb59bfe8dd0544ab6f0
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2025-11-28 10:03:53 +0700

    Menambahkan kelas DatabaseSiswa sebagai konfigurasi Room Database

---
+++ app/src/main/java/com/example/myfirstroom/room/DatabaseSiswa.kt
@@ -0,0 +1,32 @@
+package com.example.myfirstroom.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [Siswa::class],
+    version = 1,
+    exportSchema = false
+)
+
+abstract class DatabaseSiswa : RoomDatabase() {
+
+    abstract fun siswaData(): SiswaData
+
+    companion object {
+        @Volatile
+        private var Instance: com.example.myfirstroom.room.DatabaseSiswa? = null
+
+        fun getDatabase(context: Context): com.example.myfirstroom.room.DatabaseSiswa {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context,
+                    com.example.myfirstroom.room.DatabaseSiswa::class.java,
+                    "siswa_database"
+                ).build().also { Instance=it}
+            }
+        }
+    }
+}
