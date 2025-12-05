0d6cce18e297a93f58149b288cd3e3570ee03ce8
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2025-12-05 14:50:36 +0700

    Memperbaiki code typo delete dan update

--- app/src/main/java/com/example/myfirstroom/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/myfirstroom/repositori/RepositoriSiswa.kt
@@ -22,6 +22,6 @@ class OfflineRepositoriSiswa(
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
 
     override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
-    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.insert(siswa)
-    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.Update(siswa)
 }
